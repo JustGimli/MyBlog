c50ff54dd9a5c11909bc3d2f46d7a715df376586
JustGimli <aleksnermer@gmail.com>
2023-02-13 01:35:59 +0300

    add infinity scrolling

--- my-blog/src/components/Posts/PostList/List.jsx
+++ my-blog/src/components/Posts/PostList/List.jsx
@@ -5,13 +5,10 @@ import Item from "./PostItem";
 
 
 export default function PostList(props) {
-    const localPath = "http://127.0.0.1:8000/posts/"
 
     const [posts, setPosts] =useState([])
-    const [href, setHref] = useState(localPath)
+    const [href, setHref] = useState("http://127.0.0.1:8000/posts/")
     const [fetching, setFetching] = useState(true)
-    const [currentPost, setCurrentPost] = useState(1)
-    const [totalCount, setTotalCount] = useState(2)
 
     useEffect ( () => {
         document.addEventListener('scroll', scrollHandler)
@@ -23,28 +20,27 @@ export default function PostList(props) {
     
     useEffect( () => {
         if(fetching) {
-            axios.get(`${href}`)
-                .then(responce => {
-                    console.log(responce)
-                    if(localPath === href) {
+            if (href !== null)
+            {   
+                axios.get(`${href}`)
+                    .then(responce => {
+
+                        const nextPage = responce.data.next
+                        setHref(nextPage)
+                        setPosts([...posts, ...responce.data.results])
                         
-                        setTotalCount(responce.data.results[0].id)
-                        console.log(totalCount)
-                    }
-                    let nextPage = responce.data.next
-                    setCurrentPost(prev => prev + 1)
-                    setHref(nextPage)
-                    setPosts([...posts, ...responce.data.results])
-                    
-                })
-                .finally(() => setFetching(false))
+                    })
+                    .finally(() => setFetching(false))
+            }
         }
     }, [fetching])    
 
     const scrollHandler = (e) => {
-        if ((e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100)  && currentPost < totalCount) {
+        
+        if ((e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100)) {
             setFetching(true)   
         }
+        
     }
 
     return (
