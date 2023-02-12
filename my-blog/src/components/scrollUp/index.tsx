95d61e0328c3fc6749cefb33e5cc0785271a044f
JustGimli <aleksnermer@gmail.com>
2023-02-12 10:11:40 +0300

    scroll UP

--- my-blog/src/components/scrollUp/index.tsx
+++ my-blog/src/components/scrollUp/index.tsx
@@ -4,8 +4,12 @@ import React from "react";
 import {ImArrowUp} from 'react-icons/im'
 
 export default function ScrollUp(props: any) {
+    const Up = () => {
+        window.scrollTo(0,0)
+    }
+
     return (
-        <div className="ScrollUp" data-aos="scrollUp">
+        <div className="ScrollUp" data-aos="scrollUp" onClick={Up}>
             <ImArrowUp />
         </div>
     )
