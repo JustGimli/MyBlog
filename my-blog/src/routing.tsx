4727836ce13d0b6ff24acd6050f255b3cafca33d
JustGimli <aleksnermer@gmail.com>
2023-02-13 22:16:04 +0300

    Add generationg text

--- my-blog/src/routing.tsx
+++ my-blog/src/routing.tsx
@@ -4,14 +4,17 @@ import Navbar from './components/navbar';
 import Featured from './components/Main/Features';
 import Skills from './components/Main/skills/skills';
 import Contributors from "./components/Main/Contributors/contr";
-import ScrollUp from "./components/scrollUp";
+import ScrollUp from "./components/scrollUp"
+import Home from "./components/Main/home";
 
 export default function MainPage(props:any) {
     return (
         <>
             <Navbar/>
             <div className='background' style={{ "background": "#121212"}}>
-            <canvas style={{"height": "100%", "width": "100%"}}></canvas>
+            
+            <canvas style={{"height": "100%", "width": "100%", "position":"absolute"}}></canvas>
+            <Home/>
             <Featured/>
             <Skills/>
             <Contributors />
