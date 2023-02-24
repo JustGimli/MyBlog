2a5aea02382e864ecdd87a1eb4a1594825ce0624
JustGimli <aleksnermer@gmail.com>
2023-02-24 22:31:04 +0300

    Add urls

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = ([
-    path('admin/', admin.site.urls),
+    path('notforu/', admin.site.urls),
     path('', include("endpoints.urls")),
     path('token/', obtain_auth_token)
 
